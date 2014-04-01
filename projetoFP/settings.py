73bf28c076519e36a915b09b8f8e1d760509209f
franciscojunqueiralima <franciscojunqueiralima@gmail.com>
2014-03-31 21:35:21 -0300

    Acerto do settings

--- projetoFP/settings.py
+++ projetoFP/settings.py
@@ -35,7 +35,10 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles',
+    'django.contrib.staticfiles'
+    'core',
+    'pessoas',
+    'caixas',
 )
 
 MIDDLEWARE_CLASSES = (
