a4b34a3fa56f684857c325a441136fb317ec7532
franciscojunqueiralima <franciscojunqueiralima@gmail.com>
2014-04-07 21:25:36 -0300

    Listando Pessoas

--- projetoFP/settings.py
+++ projetoFP/settings.py
@@ -35,7 +35,6 @@ INSTALLED_APPS = (
     'django.contrib.contenttypes',
     'django.contrib.sessions',
     'django.contrib.messages',
-    'django.contrib.staticfiles'
     'core',
     'pessoas',
     'caixas',
@@ -68,7 +67,7 @@ DATABASES = {
 # Internationalization
 # https://docs.djangoproject.com/en/1.6/topics/i18n/
 
-LANGUAGE_CODE = 'en-us'
+LANGUAGE_CODE = 'pt-br'
 
 TIME_ZONE = 'UTC'
 
